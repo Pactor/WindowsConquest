2f724e05573fdd0842e993dc400650438fa83767
Daniel Cosper <cosperda@gmail.com>
2025-09-20 14:42:12 -0400

    Add gamedata JSON, update GameConfig, cleanup ignored dirs

--- WindowsConquest/Data/GameConfig.cs
+++ WindowsConquest/Data/GameConfig.cs
@@ -1,1 +1,23 @@
-namespace WindowsConquest.Data { public class GameConfig { } }
+using System.IO;
+
+namespace WindowsConquest.Data
+{
+    public static class GameConfig
+    {
+        public static readonly string GameDataPath =
+            Path.Combine(Directory.GetCurrentDirectory(), "gamedata");
+
+        public static readonly string AssetsPath =
+            Path.Combine(Directory.GetCurrentDirectory(), "assets");
+
+        public static string GetGameDataFile(string fileName)
+        {
+            return Path.Combine(GameDataPath, fileName);
+        }
+
+        public static string GetAssetFile(string relativePath)
+        {
+            return Path.Combine(AssetsPath, relativePath);
+        }
+    }
+}
